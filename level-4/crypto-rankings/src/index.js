ac03c8eb7b5e5f06a5c904ee38e9fc2f1bcf3d36
steverodri91 <steve.rodri91@gmail.com>
2021-01-13 15:46:17 -0500

    Add: level-4: crypto-rankings: add React router and Context Wrappers to index.js

--- level-4/crypto-rankings/src/index.js
+++ level-4/crypto-rankings/src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { CoinContextProvider } from "./context/CoinContext";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Root = () => (
+  <Router>
+    <CoinContextProvider>
+      <App />
+    </CoinContextProvider>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));

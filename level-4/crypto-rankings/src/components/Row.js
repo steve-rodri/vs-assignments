afd5b6136e43c0b5425870857a22c489830c758d
steverodri91 <steve.rodri91@gmail.com>
2021-01-21 15:51:16 -0500

    Refactor: level-4: crypto-rankings: prevent link propagation when clicking favorite button in Row

--- level-4/crypto-rankings/src/components/Row.js
+++ level-4/crypto-rankings/src/components/Row.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PercentChanges from "./PercentChanges";
 import {
   FavoriteButton,
@@ -11,14 +12,19 @@ import {
 
 const Row = props => {
   return (
-    <div className="row" style={{ display: "flex" }}>
+    <div className="row">
       <FavoriteButton {...props} />
-      <Rank {...props} />
-      <Logo {...props} />
-      <Name {...props} />
-      <Price {...props} />
-      <PercentChanges {...props} />
-      <MarketCap {...props} />
+      <Link
+        style={{ display: "flex", textDecoration: "none", color: "black" }}
+        to={`/${props.id}`}
+      >
+        <Rank {...props} />
+        <Logo {...props} />
+        <Name {...props} />
+        <Price {...props} />
+        <PercentChanges {...props} />
+        <MarketCap {...props} />
+      </Link>
     </div>
   );
 };

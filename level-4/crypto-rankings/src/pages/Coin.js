0c4607571ac2efda24051e92a32e275c197c09ce
steverodri91 <steve.rodri91@gmail.com>
2021-01-21 15:51:55 -0500

    Add: level-4: crypto-rankings: Coin Page

--- level-4/crypto-rankings/src/pages/Coin.js
+++ level-4/crypto-rankings/src/pages/Coin.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import CoinContext from "../context/CoinContext";
+import { getCoinPriceData, getCoinInfo } from "../services/nomics";
+import PercentChanges from "../components/PercentChanges";
+import {
+  FavoriteButton,
+  Rank,
+  Logo,
+  Name,
+  Price,
+  MarketCap,
+} from "../components/_coin-sub-components";
 
 const Coin = () => {
-  return <div className="Coin"></div>;
+  const [coin, setCoin] = useState({});
+  const { store, toggleFavorite } = useContext(CoinContext);
+  const { coinId } = useParams();
+  const onFavorite = e => {
+    e.stopPropagation();
+    toggleFavorite(coin);
+  };
+  useEffect(() => {
+    const fetchCoin = async () => {
+      let tmp = store.find(c => c.id === coinId);
+      if (!tmp) tmp = await getCoinPriceData(coinId);
+      const info = await getCoinInfo(coinId);
+      tmp = { ...tmp, ...info };
+      setCoin(tmp);
+    };
+    fetchCoin();
+  }, [coinId, store]);
+  return (
+    <div className="coin">
+      <FavoriteButton {...coin} onFavorite={onFavorite} />
+      <Rank {...coin} />
+      <Logo {...coin} />
+      <Name {...coin} />
+      <Price {...coin} />
+      <PercentChanges {...coin} />
+      <MarketCap {...coin} />
+    </div>
+  );
 };
-
 export default Coin;

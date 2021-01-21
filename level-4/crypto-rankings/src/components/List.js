4afde670bef3a08a9e1ee63b04a625b568981469
steverodri91 <steve.rodri91@gmail.com>
2021-01-21 15:39:16 -0500

    Refactor: level-4: crypto-rankings: List component

--- level-4/crypto-rankings/src/components/List.js
+++ level-4/crypto-rankings/src/components/List.js
@@ -3,34 +3,22 @@ import CoinContext from "../context/CoinContext";
 import Row from "./Row";
 import Card from "./Card";
 
-const List = ({ layout, ...rest }) => {
-  const context = useContext(CoinContext);
-  switch (layout) {
-    case "cards":
-      return <Cards {...rest} {...context} />;
-    default:
-      return <Table {...rest} {...context} />;
-  }
-};
-
-const Table = ({ coins, store, toggleFavorite, view }) => {
+const List = ({ view }) => {
+  let { coins, store, layout, toggleFavorite } = useContext(CoinContext);
+  const onFavorite = (e, coin) => {
+    e.stopPropagation();
+    toggleFavorite(coin);
+  };
   if (view === "favorites") coins = store.filter(coin => coin.favorite);
   return (
-    <div className="table">
-      {coins.map(coin => (
-        <Row {...coin} key={coin.id} onFavorite={() => toggleFavorite(coin)} />
-      ))}
-    </div>
-  );
-};
-
-const Cards = ({ coins, store, toggleFavorite, view }) => {
-  if (view === "favorites") coins = store.filter(coin => coin.favorite);
-  return (
-    <div className="cards">
-      {coins.map(coin => (
-        <Card {...coin} key={coin.id} onFavorite={() => toggleFavorite(coin)} />
-      ))}
+    <div className={layout === "cards" ? "cards" : "table"}>
+      {coins.map(coin =>
+        layout === "cards" ? (
+          <Card {...coin} key={coin.id} onFavorite={e => onFavorite(e, coin)} />
+        ) : (
+          <Row {...coin} key={coin.id} onFavorite={e => onFavorite(e, coin)} />
+        )
+      )}
     </div>
   );
 };

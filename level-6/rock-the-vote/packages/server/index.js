dc5107dc4aa000fbd5d7424d440e93900db97784
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-02 14:37:34 -0500

    update server/index to use database file and change comments route to comment route

--- level-6/rock-the-vote/packages/server/index.js
+++ level-6/rock-the-vote/packages/server/index.js
@@ -2,22 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const jwt = require("express-jwt");
-const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 const app = express();
-const PORT = 4000;
 
-mongoose.connect(
-  `mongodb://localhost:27017/rock-the-vote`,
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  },
-  () => console.log("connected to mongo")
-);
+require("./database");
 
 dotenv.config();
 app.use(express.json());
@@ -38,4 +27,7 @@ app.use((err, _, res) => {
   res.send({ error: err.message });
 });
 
-app.listen(PORT, console.log(`server running on ${PORT}`));
+app.listen(
+  process.env.PORT,
+  console.log(`server listening on ${process.env.PORT}`)
+);

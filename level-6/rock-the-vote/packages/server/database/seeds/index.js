07866d9f7f3769ce61911459ce0efbce7b2cf419
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-02 14:36:01 -0500

    add seeds index file

---
+++ level-6/rock-the-vote/packages/server/database/seeds/index.js
@@ -0,0 +1,9 @@
+const seedUsers = require("./user");
+const seedComments = require("./comment");
+const seedIssues = require("./issue");
+
+module.exports = {
+  seedUsers,
+  seedComments,
+  seedIssues,
+};

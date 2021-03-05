08e747af01f1d7f4b6673f1083365ea234d7e187
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-05 14:31:15 -0500

    add userSchema methods to hash and verify passwords, also add some validation

--- level-6/rock-the-vote/packages/server/models/user.js
+++ level-6/rock-the-vote/packages/server/models/user.js
@@ -1,5 +1,6 @@
 const { model, Schema } = require("mongoose");
 const bcrypt = require("bcrypt");
+const HTTPError = require("../services/HTTPError");
 
 const userSchema = new Schema({
   username: {
@@ -10,28 +11,36 @@ const userSchema = new Schema({
   password: {
     type: String,
     required: true,
+    minLength: 8,
   },
 });
+userSchema.methods.verify = async function comparePassword(attempt) {
+  return bcrypt.compare(attempt, this.password);
+};
 
+userSchema.methods.withoutPassword = function removePassword() {
+  const { password, ...rest } = this.toObject();
+  return rest;
+};
+
+// ensure username is lowercase
+userSchema.path("username").validate = {
+  validator: name => {
+    if (name !== name.toLowerCase()) {
+      throw new HTTPError(403, "username must be lowercase");
+    }
+  },
+  message: props => props.reason.message,
+};
+
+// hash password pre save
 userSchema.pre("save", async function hashPassword(next) {
   try {
     if (!this.isModified("password")) next();
-    const hash = await bcrypt.hash(this.password, 10);
-    this.password = hash;
+    this.password = await bcrypt.hash(this.password, 10);
   } catch (err) {
     next(err);
   }
 });
-
-userSchema.methods.checkPassword = async function comparePassword(attempt) {
-  return bcrypt.compare(attempt, this.password);
-};
-
-// method to remove user's password for token/sending the response
-userSchema.methods.withoutPassword = function removePassword() {
-  const user = this.Object();
-  delete user.password;
-  return user;
-};
 
 module.exports = model("User", userSchema);

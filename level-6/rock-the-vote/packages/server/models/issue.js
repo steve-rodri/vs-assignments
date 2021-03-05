e16025902c13e79a4296b6767d7f82c8c76c8947
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-05 14:32:46 -0500

    add a pre hook to issueSchema to populate creator without password

--- level-6/rock-the-vote/packages/server/models/issue.js
+++ level-6/rock-the-vote/packages/server/models/issue.js
@@ -21,12 +21,21 @@ const issueSchema = new Schema({
   downvotedUsers: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
+issueSchema.methods.withoutVoterArrays = function removeArrays() {
+  const { upvotedUsers, downvotedUsers, ...rest } = this.toObject();
+  return rest;
+};
+
 issueSchema.set("toJSON", { virtuals: true });
 issueSchema.virtual("votes").get(function virtualVotes() {
   const up = this.upvotedUsers.length;
   const down = this.downvotedUsers.length;
   const total = up + down;
   return { up, down, total };
+});
+
+issueSchema.pre(/^(save|find|update)/, function popCreator() {
+  this.populate({ path: "creator", select: "-password" });
 });
 
 issueSchema.plugin(require("mongoose-autopopulate"));

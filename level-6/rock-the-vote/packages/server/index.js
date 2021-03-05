efb8810e1284709a8b2469f76534a2f784c830c4
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-05 14:28:21 -0500

    return status of error in main error handler

--- level-6/rock-the-vote/packages/server/index.js
+++ level-6/rock-the-vote/packages/server/index.js
@@ -8,6 +8,8 @@ require("./routes")(app);
 
 app.use((err, _, res) => {
   console.log(err); // eslint-disable-line
+  res.status(500);
+  if (err.status) res.status(err.status);
   res.send({ error: err.message });
 });
 

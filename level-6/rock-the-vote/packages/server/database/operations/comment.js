c5e6d8335432a0a6af4538ccf6fe22df4c5b444c
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-05 14:46:34 -0500

    add more comment operations, refactor comment routes

--- level-6/rock-the-vote/packages/server/database/operations/comment.js
+++ level-6/rock-the-vote/packages/server/database/operations/comment.js
@@ -1,5 +1,7 @@
-const { Comment } = require("../../models");
+const HTTPError = require("../../services/HTTPError");
 const randomNum = require("../../services/randomNum");
+const { Comment } = require("../../models");
+const { addCommentToIssue, removeCommentFromIssue } = require("./issue");
 
 const getRandomComments = async (min, max) => {
   const amount = randomNum(min, max);
@@ -7,6 +9,29 @@ const getRandomComments = async (min, max) => {
   return comments.map(c => c._id);
 };
 
+const createComment = async ({ body, user }) => {
+  const { issueId, ...rest } = body;
+  if (!issueId) throw new HTTPError(403, "No issueId");
+  const data = { ...rest, creator: user._id };
+  const comment = await Comment.create(data);
+  await addCommentToIssue(issueId, comment);
+};
+
+const updateComment = async (id, { body, user }) => {
+  const filter = { _id: id, creator: user._id };
+  const comment = await Comment.findOneAndUpdate(filter, body);
+  if (!comment) throw new HTTPError(403, "Cannot update unowned comment");
+};
+
+const deleteComment = async (id, { body, user }) => {
+  if (!body.issueId) throw new HTTPError(403, "No issueId");
+  await Comment.findOneAndDelete({ _id: id, creator: user._id });
+  await removeCommentFromIssue(body.issueId, id);
+};
+
 module.exports = {
+  createComment,
+  updateComment,
+  deleteComment,
   getRandomComments,
 };
--- level-6/rock-the-vote/packages/server/routes/comment.js
+++ level-6/rock-the-vote/packages/server/routes/comment.js
@@ -1,19 +1,38 @@
 const { Router } = require("express");
-const Comment = require("../models/comment");
+const {
+  createComment,
+  updateComment,
+  deleteComment,
+} = require("../database/operations");
+
+const create = async (req, res, next) => {
+  try {
+    const comment = await createComment(req);
+    res.status(200).send(comment);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const remove = async (req, res, next) => {
+  try {
+    const comment = await deleteComment(req.params.id, req.user);
+    res.status(200).send(comment);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const update = async (req, res, next) => {
+  try {
+    const comment = await updateComment(req.params.id, req.user);
+    res.status(200).send(comment);
+  } catch (err) {
+    next(err);
+  }
+};
 
 const router = Router();
-
-router.route("/comments/:id").put((req, res, next) => {
-  Comment.findOneAndUpdate(
-    { _id: req.params.id, creator: req.user._id },
-    req.body,
-    { new: true }
-  )
-    .then(comment => res.status(202).send(comment))
-    .catch(err => {
-      res.status(500);
-      return next(err);
-    });
-});
-
+router.route("/comments").post(create);
+router.route("/comments/:id").put(update).delete(remove);
 module.exports = router;

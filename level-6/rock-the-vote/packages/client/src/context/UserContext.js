abb9a1dd8e048d606945508f91785fbbc54ffc48
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-10 18:25:31 -0500

    add User Context

---
+++ level-6/rock-the-vote/packages/client/src/context/UserContext.js
@@ -0,0 +1,40 @@
+import { createContext, useState, useEffect } from "react";
+import { createUser, getUser } from "../services/api-auth";
+import { loadState, saveState } from "../helpers/persistState";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const persistedState = loadState("user");
+  const initialState = persistedState || { user: {}, token: "" };
+  const [userData, setUserData] = useState(initialState);
+
+  const signup = async credentials => {
+    const data = await createUser(credentials);
+    setUserData(data);
+  };
+
+  const login = async credentials => {
+    const data = await getUser(credentials);
+    setUserData(data);
+  };
+
+  const logout = () => {
+    setUserData(initialState);
+  };
+
+  useEffect(() => {
+    saveState(userData, "user");
+  }, [userData]);
+
+  const value = {
+    ...userData,
+    signup,
+    login,
+    logout,
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+
+export default UserContext;

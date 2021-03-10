6f0e669537053ebdc4fcd148aa89b468e852fbe0
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-10 18:36:53 -0500

    install react-router-dom, add Router and User Providers to Root

--- level-6/rock-the-vote/packages/client/src/index.js
+++ level-6/rock-the-vote/packages/client/src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { UserProvider } from "./context/UserContext";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Root = () => (
+  <Router>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </Router>
+);
+
+ReactDOM.render(<Root />, document.getElementById("root"));

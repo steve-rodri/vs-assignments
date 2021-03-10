91afb8245e8eba7db3fcef2dff820fa45dd57ab5
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-10 18:38:17 -0500

    create route components, initialize pages

--- level-6/rock-the-vote/packages/client/src/components/App.js
+++ level-6/rock-the-vote/packages/client/src/components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { Home, Login, Detail } from "../pages";
+import { ProtectedRoute, UnknownRoute } from "./routes";
 
 const App = () => {
-  return <div className="App">Client</div>;
+  return (
+    <Switch>
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute path="/:id" component={Detail} />
+      <Route to="/login" component={Login} />
+      <UnknownRoute />
+    </Switch>
+  );
 };
 
 export default App;
---
+++ level-6/rock-the-vote/packages/client/src/pages/Detail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Detail = () => {
+  return <div className="Detail">Detail</div>;
+};
+
+export default Detail;

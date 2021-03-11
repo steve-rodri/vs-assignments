f07c18d61c43cc99464c6a0da7046de783040c61
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-10 23:45:11 -0500

    add chakra-ui, formik, and yup

--- level-6/rock-the-vote/packages/client/src/index.js
+++ level-6/rock-the-vote/packages/client/src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { UserProvider } from "./context/UserContext";
 import { IssueProvider } from "./context/IssueContext";
 import App from "./components/App";
 
+const theme = extendTheme({ config: { initialColorMode: "dark" } });
+
 const Root = () => (
   <Router>
-    <UserProvider>
-      <IssueProvider>
-        <App />
-      </IssueProvider>
-    </UserProvider>
+    <ChakraProvider theme={theme}>
+      <UserProvider>
+        <IssueProvider>
+          <App />
+        </IssueProvider>
+      </UserProvider>
+    </ChakraProvider>
   </Router>
 );
 

e3e9f440043274be088c830f5ea749443e5ce4a3
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-18 09:59:15 -0400

    add issue Detail page

--- level-6/rock-the-vote/packages/client/src/pages/Detail.js
+++ level-6/rock-the-vote/packages/client/src/pages/Detail.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Redirect } from "react-router-dom";
+import Header from "../components/PageHeader";
+import IssueDetail from "../components/issue/Detail";
+import IssueContext from "../context/IssueContext";
+import { Grid } from "@chakra-ui/react";
 
-const Detail = () => {
-  return <div className="Detail">Detail</div>;
+const Detail = ({ match }) => {
+  const { issues } = useContext(IssueContext);
+  const issue = issues.find(issue => issue._id === match.params.id);
+  if (!issue) return <Redirect to="/" />;
+  return (
+    <Grid>
+      <Header />
+      <IssueDetail {...issue} />
+    </Grid>
+  );
 };
 
 export default Detail;

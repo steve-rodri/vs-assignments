3e70354b45fc09e4a6be5b7e16c73871655865f8
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-18 19:45:03 -0400

    add more seeds, refactor seeder

--- level-6/rock-the-vote/packages/server/database/reset.js
+++ level-6/rock-the-vote/packages/server/database/reset.js
@@ -2,14 +2,14 @@ require("."); // instantiate the database connection
 const { connection } = require("mongoose");
 const { seedUsers, seedComments, seedIssues } = require("./seeds");
 
-// drop database
+// drop the database
 connection.dropDatabase();
 
 const seedDatabase = async () => {
   try {
-    await seedUsers(20);
-    await seedComments(100);
-    await seedIssues(50);
+    await seedUsers(40);
+    await seedComments(150);
+    await seedIssues(100);
     console.log("Database Seeded Successfully"); //eslint-disable-line
     connection.close();
   } catch (err) {
--- level-6/rock-the-vote/packages/server/database/seeder.js
+++ level-6/rock-the-vote/packages/server/database/seeder.js
@@ -2,8 +2,7 @@ const { connection } = require("mongoose");
 
 module.exports = async ({ count, generateDoc, model: Model, plural }) => {
   console.log(`Seeding ${count} ${plural} into ${connection.name}`); // eslint-disable-line
-  const docs = await Promise.all(
-    Array.from({ length: count }).map(async () => generateDoc())
-  );
+  const genDocs = Array.from({ length: count }).map(async () => generateDoc());
+  const docs = await Promise.all(genDocs);
   await Model.insertMany(docs);
 };

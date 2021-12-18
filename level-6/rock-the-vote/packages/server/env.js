1148b73831ad87583397ffda8070edb7d5e9ef2f
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-12-18 17:21:19 -0500

    Update: level-6: rock-the-vote: add login to deploy script, update env variables

--- level-6/rock-the-vote/packages/server/env.js
+++ level-6/rock-the-vote/packages/server/env.js
@@ -2,9 +2,14 @@ const { config } = require("dotenv");
 
 config();
 
+const env = process.env.NODE_ENV || "development";
+
 module.exports = {
   secret: process.env.SECRET,
-  db: process.env.MONGO_DB_URI,
-  clientUrl: process.env.CLIENT_URL,
+  db: process.env[`MONGO_DB_${env.toUpperCase()}_URI`],
+  clientUrl: process.env[`CLIENT_${env.toUpperCase()}_URL`],
   port: process.env.PORT,
 };
+
+// To deploy to Heroku you must configure environment variables on their website
+// or using the CLI
--- level-6/rock-the-vote/packages/server/utils/deploy.zsh
+++ level-6/rock-the-vote/packages/server/utils/deploy.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/ zsh
 
+heroku login
 git add --all
 git commit -m "new production build"
 git push production main
